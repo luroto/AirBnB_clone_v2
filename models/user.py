8d5801f376e8466b5125cbdb5e029835b8cfee14
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-15 21:26:57 +0000

    [Feature] Uploading the User class with DBEngine attributes

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel):
     """This is the class for user
@@ -11,7 +12,13 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
+    __tablename__ = 'users'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
     password = ""
     first_name = ""
     last_name = ""
