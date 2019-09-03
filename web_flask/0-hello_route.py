ff6ce0bcf4450de80263a8fa2ca201c727ed1004
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-03 21:00:24 +0000

    [Fix] Trying to work even if it's imported

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,8 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
-
+app.run(host='0.0.0.0', port=5000)
 @app.route('/', strict_slashes=False)
 def home():
     """This section defines content for home route"""
     return "Hello HBNHB!"
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
