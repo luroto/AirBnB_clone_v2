89ba241c202bb2750eb73a56ca57058ea7001dd1
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-03 21:11:39 +0000

    [Fix] Redefining all route definition by strict_slashes=false

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,12 @@ This script starts a Flask web application
 from flask import Flask, escape, request
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-app.run(host='0.0.0.0', port=5000)
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def home():
     """This section defines content for home route"""
     return "Hello HBNHB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
