4bc868d7afc3c33f5515bd243b05bd5f95f0fe11
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-03 22:42:31 +0000

    [Feature]Uploading the third file

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application with some routes
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """This section defines content for home route"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def hbnb():
+        """This section defines for hbnb route """
+        return ("HBNB")
+
+
+@app.route('/c/<text>')
+def c(text):
+        """ This section defines for routes for variables"""
+        if "_" in text:
+            text = text.replace("_", " ")
+        return ("C {}".format(text))
+
+
+@app.route('/python/<text>')
+@app.route('/python')
+def python(text="is cool"):
+        """ This section defines proceedure for python routes"""
+        if "_" in text:
+            text = text.replace("_", " ")
+        return("Python {}".format(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
