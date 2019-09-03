2cebdee4747c565bcee807cdfc015bd209baea21
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-03 20:40:07 +0000

    [Feature] Uploading the zero task

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application
+"""
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    """This section defines content for home route"""
+    return "Hello HBNHB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
