83fde074c123110b87b26516213690c0941929ee
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-03 20:52:18 +0000

    [Fix] ! added

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def home():
     """This section defines content for home route"""
-    return "Hello HBNHB"
+    return "Hello HBNHB!"
 
 
 if __name__ == '__main__':
